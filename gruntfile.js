c1c9d33969bf8978d24f7f90ed78a672a45a4e24
vzerwas <vzerwas@epages.com>
2014-02-26 12:15:42 +0100

    added JS lint grunt task

--- gruntfile.js
+++ gruntfile.js
@@ -1,72 +1,85 @@
-module.exports = function(grunt) {
+/*global module*/
+module.exports = function (grunt) {
+    'use strict';
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-connect');
+    grunt.loadNpmTasks('grunt-processhtml');
+    grunt.loadNpmTasks('grunt-htmlhint');
+    grunt.loadNpmTasks('grunt-jslint');
 
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-less');
-  grunt.loadNpmTasks('grunt-contrib-connect');
-  grunt.loadNpmTasks('grunt-processhtml');
-  grunt.loadNpmTasks('grunt-htmlhint');
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
 
-  // Project configuration.
-  grunt.initConfig({
-      pkg: grunt.file.readJSON('package.json'),
-
-      less: {
-        development: {
-          files: {
-            "public/css/style.css": "app/less/style.less"
-          }
-        }
-      },
-
-      connect: {
-        server: {
-          options: {
-            // bei weglassen, standardmäßig auf port 8000
-            port: 9001,
-            // ist ausgangsordner von index.html
-            base: 'public',
-            livereload: true
-          }
-        }
-      },
-
-      processhtml: {
-        dist: {
-          files: {
-            'public/index.html': ['app/index.html']
-          }
-        }
-      },
-
-      htmlhint: {
-        html1: {
-          options: {
-            'tag-pair': true
-          },
-          src: ['public/**/*.html']
-        }
-      },
-
-      // watch task
-      watch: {
-        options: {
-          livereload: true,
-        },
         less: {
-          files: ['app/less/**/*.less'],
-          tasks: ['less']
+            development: {
+                files: {
+                    "public/css/style.css": "app/less/style.less"
+                }
+            }
         },
+
+        connect: {
+            server: {
+                options: {
+                    // bei weglassen, standardmäßig auf port 8000
+                    port: 9001,
+                    // ist ausgangsordner von index.html
+                    base: 'public',
+                    livereload: true
+                }
+            }
+        },
+
         processhtml: {
-          files: ['app/index.html'],
-          tasks: ['processhtml']
+            dist: {
+                files: {
+                    'public/index.html': ['app/index.html']
+                }
+            }
         },
+
         htmlhint: {
-          files: ['public/index.html'],
-          tasks: ['htmlhint']
+            html1: {
+                options: {
+                    'tag-pair': true
+                },
+                src: ['public/**/*.html']
+            }
+        },
+
+        jslint: { // configure the task
+        // lint your project's server code
+            server: {
+                src: ['gruntfile.js']
+            }
+        },
+
+        // watch task
+        watch: {
+            options: {
+                livereload: true
+            },
+            less: {
+                files: ['app/less/**/*.less'],
+                tasks: ['less']
+            },
+            processhtml: {
+                files: ['app/index.html'],
+                tasks: ['processhtml']
+            },
+            htmlhint: {
+                files: ['public/index.html'],
+                tasks: ['htmlhint']
+            },
+            jslint: {
+                files: ['gruntfile.js'],
+                tasks: ['jslint']
+            }
         }
-      }
-  });
-  // Default task(s).
-  grunt.registerTask('default',['connect', 'watch']);
+    });
+    // Default task(s).
+    grunt.registerTask('default', ['connect', 'watch']);
 
 };
